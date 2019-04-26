d70ca7fe614c6255f545e818dc1126b3c5f6fda1
andycall <dongtiangche@outlook.com>
2019-04-26 18:42:07 +0800

    feat(core): Test Tools can find ES Component with getComponentByType API

--- packages/rcre-test-tools/src/RCRETestUtil.tsx
+++ packages/rcre-test-tools/src/RCRETestUtil.tsx
@@ -1,25 +1,10 @@
 import React from 'react';
-import {get, flatten} from 'lodash';
-import chalk from 'chalk';
+import {get, } from 'lodash';
 import {mount, ReactWrapper} from 'enzyme';
 import {ReactElement} from 'react';
-import {PageConfig, JSONRender, RootState, vm, RCREState} from 'rcre';
+import {PageConfig, JSONRender, RootState, RCREState} from 'rcre';
 import {Store} from 'redux';
-
-const format = require('json-format');
-
-interface DebugOptions {
-    // 显示原始ExpressionString内容
-    raw?: boolean;
-    // 输出dataProvider
-    showDataProvider?: boolean;
-    // 输出dataCustomer
-    showDataCustomer?: boolean;
-    // 输出props
-    showProps?: boolean;
-    // 输出export
-    showExport?: boolean;
-}
+import {ExecTaskOptions} from '../../rcre/src/types';
 
 export class RCRETestUtil {
     public config: PageConfig<any>;
@@ -179,7 +164,19 @@ export class RCRETestUtil {
 
         let elements: ReactWrapper[] = [];
         for (let i = 0; i < container.children().length; i++) {
-            this.find(container.childAt(i), elements, 'RCREConnect(' + type + ')');
+            this.find(container.childAt(i), elements, (element) => {
+                let name = element.name();
+
+                if (name === 'RCREConnect(' + type + ')') {
+                    return true;
+                }
+
+                if (name === 'ES' && element.prop('type') === type) {
+                    return true;
+                }
+
+                return false;
+            });
         }
 
         if (elements.length === 0) {
@@ -209,7 +206,19 @@ export class RCRETestUtil {
 
         let elements: ReactWrapper[] = [];
         for (let i = 0; i < container.children().length; i++) {
-            this.find(container.childAt(i), elements, 'RCREConnect(' + type + ')');
+            this.find(container.childAt(i), elements, element => {
+                let name = element.name();
+
+                if (name === 'RCREConnect(' + type + ')') {
+                    return true;
+                }
+
+                if (name === 'ES' && element.prop('type') === type) {
+                    return true;
+                }
+
+                return false;
+            });
         }
 
         if (elements.length === 0) {
@@ -234,6 +243,10 @@ export class RCRETestUtil {
         for (let i = 0; i < container.children().length; i++) {
             this.find(container.childAt(i), elements, (element) => {
                 if (/RCREConnect\(.*\)/.test(element.name()) && element.prop('name') === name) {
+                    return true;
+                }
+
+                if (element.name() === 'ES' && element.prop('name') === name) {
                     return true;
                 }
 
@@ -406,6 +419,19 @@ export class RCRETestUtil {
     }
 
     /**
+     * 触发组件所附属Container组件的一个Task or TaskGroup
+     * @param component
+     * @param task
+     * @param args
+     * @param options
+     */
+    public async execTask(component: ReactWrapper, task: string, args: any, options: ExecTaskOptions = {}) {
+        let instance: any = component.instance();
+        await instance.TEST_execTask(task, args, options);
+        this.wrapper.update();
+    }
+
+    /**
      * 触发trigger中某一个事件
      * @param component
      * @param event
@@ -473,221 +499,221 @@ export class RCRETestUtil {
         return instance.TEST_getNameValue(info.name);
     }
 
-    /**
-     * 输出当前组件和所有子组件的结构
-     * @param node {ReactWrapper} 根节点
-     * @param options {DebugOptions} 输入配置
-     */
-    public debug(node?: ReactWrapper, options?: DebugOptions): string {
-        if (!this.model) {
-            throw new Error('please set your container first');
-        }
+    // /**
+    //  * 输出当前组件和所有子组件的结构
+    //  * @param node {ReactWrapper} 根节点
+    //  * @param options {DebugOptions} 输入配置
+    //  */
+    // public debug(node?: ReactWrapper, options?: DebugOptions): string {
+    //     if (!this.model) {
+    //         throw new Error('please set your container first');
+    //     }
+    //
+    //     let container = this.getContainer(this.model);
+    //     if (node) {
+    //         container = node;
+    //     }
+    //
+    //     // @ts-ignore
+    //     let nodeInternal = container.getNodesInternal()[0];
+    //     let lines: string[] = [];
+    //     let comments: {
+    //         [type: string]: string;
+    //     } = {};
+    //     this.debugNode(nodeInternal, 0, lines, comments, options);
+    //     return lines.join('\n') + '\n' + '\n' + this.formatComments(comments);
+    // }
 
-        let container = this.getContainer(this.model);
-        if (node) {
-            container = node;
-        }
+    // private formatComments(comments: { [type: string]: string }) {
+    //     let str = '';
+    //
+    //     Object.keys(comments).forEach(key => {
+    //         str += chalk.cyan(key + 'Config') + ': ' + format(JSON.parse(comments[key]));
+    //
+    //         str += '\n';
+    //     });
+    //
+    //     return str;
+    // }
 
-        // @ts-ignore
-        let nodeInternal = container.getNodesInternal()[0];
-        let lines: string[] = [];
-        let comments: {
-            [type: string]: string;
-        } = {};
-        this.debugNode(nodeInternal, 0, lines, comments, options);
-        return lines.join('\n') + '\n' + '\n' + this.formatComments(comments);
-    }
+    // private lineIndent(depth: number) {
+    //     let str = '';
+    //
+    //     for (let i = 0; i < depth; i++) {
+    //         str += '  ';
+    //     }
+    //
+    //     return str;
+    // }
+    //
+    // private getRawNodeInfo(node: any) {
+    //     let type = node.type.displayName;
+    //
+    //     // Driver组件，读取Connect信息
+    //     if (/RCRE\(\w+\)/.test(type)) {
+    //         return node.instance.props.tools.env.info;
+    //     }
+    //
+    //     // 其余情况
+    //     return node.props.info;
+    // }
+    //
+    // private isRCREConfig(value: any) {
+    //     let valid = false;
+    //     if (value instanceof Array) {
+    //         valid = value.every(v => v.hasOwnProperty('type'));
+    //     } else {
+    //         valid = value.hasOwnProperty('type');
+    //     }
+    //
+    //     return valid;
+    // }
 
-    private formatComments(comments: { [type: string]: string }) {
-        let str = '';
-
-        Object.keys(comments).forEach(key => {
-            str += chalk.cyan(key + 'Config') + ': ' + format(JSON.parse(comments[key]));
-
-            str += '\n';
-        });
-
-        return str;
-    }
-
-    private lineIndent(depth: number) {
-        let str = '';
-
-        for (let i = 0; i < depth; i++) {
-            str += '  ';
-        }
-
-        return str;
-    }
-
-    private getRawNodeInfo(node: any) {
-        let type = node.type.displayName;
-
-        // Driver组件，读取Connect信息
-        if (/RCRE\(\w+\)/.test(type)) {
-            return node.instance.props.tools.env.info;
-        }
-
-        // 其余情况
-        return node.props.info;
-    }
-
-    private isRCREConfig(value: any) {
-        let valid = false;
-        if (value instanceof Array) {
-            valid = value.every(v => v.hasOwnProperty('type'));
-        } else {
-            valid = value.hasOwnProperty('type');
-        }
-
-        return valid;
-    }
-
-    private debugNode(node: any, depth: number, lines: string[], comments: { [type: string]: string }, options?: DebugOptions) {
-        if (!node) {
-            return;
-        }
-
-        let rendered = Array.isArray(node.rendered) ? flatten(node.rendered) : [node.rendered];
-        let nextDepth = depth;
-
-        if (node && node.instance) {
-            let info = (options && options.raw) ? this.getRawNodeInfo(node) : node.instance.TEST_INFO;
-            let type = info.type;
-
-            let rawInfo = this.getRawNodeInfo(node);
-            let compiledProps = Object.keys(rawInfo).filter(i => {
-                return vm.isExpression(rawInfo[i]);
-            });
-
-            if (!type) {
-                return;
-            }
-
-            let propStr: string = '';
-
-            Object.keys(info).forEach(key => {
-                let value = info[key];
-
-                if (typeof value === 'string') {
-                    value = '"' + value + '"';
-                }
-
-                if (typeof value === 'object' && value !== null) {
-                    if (this.isRCREConfig(value)) {
-                        return;
-                    }
-
-                    value = JSON.stringify(value).replace(/[\s\n\t]/g, '');
-                    // 超长的内容单独在最下面的显示
-                    if (value.length > 240) {
-                        if (options) {
-                            if (options.showExport && key === 'export') {
-                                comments[key] = value;
-                            }
-
-                            if (options.showProps && key === 'props') {
-                                comments[key] = value;
-                            }
-
-                            if (options.showDataCustomer && key === 'dataCustomer') {
-                                comments[key] = value;
-                            }
-
-                            if (options.showDataProvider && key === 'dataProvider') {
-                                comments[key] = value;
-                            }
-                        }
-
-                        value = chalk.cyan(`[${key}Config]`);
-                    }
-                }
-
-                if (typeof value === 'function') {
-                    return;
-                }
-
-                switch (key) {
-                    case 'name':
-                        propStr += ' ' + chalk.blue(`name=${value}`);
-                        break;
-                    case 'dataProvider':
-                    case 'dataCustomer':
-                    case 'props':
-                    case 'export': {
-                        propStr += chalk.bold(` ${key}=${value}`);
-                        break;
-                    }
-                    case 'type':
-                    case 'children':
-                        break;
-                    default: {
-                        // 把使用表达式解析的值变成原谅绿
-                        if (compiledProps.indexOf(key) >= 0) {
-                            propStr += chalk.green(` ${key}=${value}`);
-                            break;
-                        }
-
-                        propStr += ` ${key}=${value}`;
-                    }
-                }
-            });
-
-            lines.push(this.lineIndent(depth) + `${type}${propStr}`);
-
-            if (rendered.length > 0) {
-                nextDepth++;
-            }
-        }
-
-        // 隐藏的元素
-        if (node && node.props && node.props.className === 'rcre-hidden-element') {
-            let hiddenInfo = JSON.parse(node.rendered[0]);
-            this.walkHiddenInfo(hiddenInfo, depth, lines);
-        }
-
-        rendered.forEach(n => this.debugNode(n, nextDepth, lines, comments, options));
-
-        // if (node && node.instance && node.instance.TEST_INFO) {
-        //     let type = node.instance.TEST_INFO.type;
-        //     lines.push(this.lineIndent(depth) + `</${type}>`);
-        // }
-    }
-
-    private walkHiddenInfo(hiddenInfo: any, depth: number, lines: string[]) {
-        let type = hiddenInfo.type;
-
-        let propStr: string = '';
-        Object.keys(hiddenInfo).forEach(propKey => {
-            let value = hiddenInfo[propKey];
-
-            if (this.isRCREConfig(value)) {
-                return;
-            }
-
-            switch (propKey) {
-                case 'type':
-                case 'children':
-                    break;
-                default:
-                    propStr += ` ${propKey}=${value}`;
-            }
-        });
-
-        lines.push(this.lineIndent(depth) + chalk.gray(`${type}${propStr}`));
-
-        Object.keys(hiddenInfo).forEach(propKey => {
-            if (this.isRCREConfig(hiddenInfo[propKey])) {
-                let rcreConfig = hiddenInfo[propKey];
-
-                if (rcreConfig instanceof Array) {
-                    rcreConfig.forEach(config => this.walkHiddenInfo(config, depth + 1, lines));
-                } else {
-                    this.walkHiddenInfo(rcreConfig, depth + 1, lines);
-                }
-            }
-        });
-    }
+    // private debugNode(node: any, depth: number, lines: string[], comments: { [type: string]: string }, options?: DebugOptions) {
+    //     if (!node) {
+    //         return;
+    //     }
+    //
+    //     let rendered = Array.isArray(node.rendered) ? flatten(node.rendered) : [node.rendered];
+    //     let nextDepth = depth;
+    //
+    //     if (node && node.instance) {
+    //         let info = (options && options.raw) ? this.getRawNodeInfo(node) : node.instance.TEST_INFO;
+    //         let type = info.type;
+    //
+    //         let rawInfo = this.getRawNodeInfo(node);
+    //         let compiledProps = Object.keys(rawInfo).filter(i => {
+    //             return vm.isExpression(rawInfo[i]);
+    //         });
+    //
+    //         if (!type) {
+    //             return;
+    //         }
+    //
+    //         let propStr: string = '';
+    //
+    //         Object.keys(info).forEach(key => {
+    //             let value = info[key];
+    //
+    //             if (typeof value === 'string') {
+    //                 value = '"' + value + '"';
+    //             }
+    //
+    //             if (typeof value === 'object' && value !== null) {
+    //                 if (this.isRCREConfig(value)) {
+    //                     return;
+    //                 }
+    //
+    //                 value = JSON.stringify(value).replace(/[\s\n\t]/g, '');
+    //                 // 超长的内容单独在最下面的显示
+    //                 if (value.length > 240) {
+    //                     if (options) {
+    //                         if (options.showExport && key === 'export') {
+    //                             comments[key] = value;
+    //                         }
+    //
+    //                         if (options.showProps && key === 'props') {
+    //                             comments[key] = value;
+    //                         }
+    //
+    //                         if (options.showDataCustomer && key === 'dataCustomer') {
+    //                             comments[key] = value;
+    //                         }
+    //
+    //                         if (options.showDataProvider && key === 'dataProvider') {
+    //                             comments[key] = value;
+    //                         }
+    //                     }
+    //
+    //                     value = chalk.cyan(`[${key}Config]`);
+    //                 }
+    //             }
+    //
+    //             if (typeof value === 'function') {
+    //                 return;
+    //             }
+    //
+    //             switch (key) {
+    //                 case 'name':
+    //                     propStr += ' ' + chalk.blue(`name=${value}`);
+    //                     break;
+    //                 case 'dataProvider':
+    //                 case 'dataCustomer':
+    //                 case 'props':
+    //                 case 'export': {
+    //                     propStr += chalk.bold(` ${key}=${value}`);
+    //                     break;
+    //                 }
+    //                 case 'type':
+    //                 case 'children':
+    //                     break;
+    //                 default: {
+    //                     // 把使用表达式解析的值变成原谅绿
+    //                     if (compiledProps.indexOf(key) >= 0) {
+    //                         propStr += chalk.green(` ${key}=${value}`);
+    //                         break;
+    //                     }
+    //
+    //                     propStr += ` ${key}=${value}`;
+    //                 }
+    //             }
+    //         });
+    //
+    //         lines.push(this.lineIndent(depth) + `${type}${propStr}`);
+    //
+    //         if (rendered.length > 0) {
+    //             nextDepth++;
+    //         }
+    //     }
+    //
+    //     // 隐藏的元素
+    //     if (node && node.props && node.props.className === 'rcre-hidden-element') {
+    //         let hiddenInfo = JSON.parse(node.rendered[0]);
+    //         this.walkHiddenInfo(hiddenInfo, depth, lines);
+    //     }
+    //
+    //     rendered.forEach(n => this.debugNode(n, nextDepth, lines, comments, options));
+    //
+    //     // if (node && node.instance && node.instance.TEST_INFO) {
+    //     //     let type = node.instance.TEST_INFO.type;
+    //     //     lines.push(this.lineIndent(depth) + `</${type}>`);
+    //     // }
+    // }
+    //
+    // private walkHiddenInfo(hiddenInfo: any, depth: number, lines: string[]) {
+    //     let type = hiddenInfo.type;
+    //
+    //     let propStr: string = '';
+    //     Object.keys(hiddenInfo).forEach(propKey => {
+    //         let value = hiddenInfo[propKey];
+    //
+    //         if (this.isRCREConfig(value)) {
+    //             return;
+    //         }
+    //
+    //         switch (propKey) {
+    //             case 'type':
+    //             case 'children':
+    //                 break;
+    //             default:
+    //                 propStr += ` ${propKey}=${value}`;
+    //         }
+    //     });
+    //
+    //     lines.push(this.lineIndent(depth) + chalk.gray(`${type}${propStr}`));
+    //
+    //     Object.keys(hiddenInfo).forEach(propKey => {
+    //         if (this.isRCREConfig(hiddenInfo[propKey])) {
+    //             let rcreConfig = hiddenInfo[propKey];
+    //
+    //             if (rcreConfig instanceof Array) {
+    //                 rcreConfig.forEach(config => this.walkHiddenInfo(config, depth + 1, lines));
+    //             } else {
+    //                 this.walkHiddenInfo(rcreConfig, depth + 1, lines);
+    //             }
+    //         }
+    //     });
+    // }
 }
 
 export async function simulate(wrapper: any, component: any, event: string, args: object = {}) {
