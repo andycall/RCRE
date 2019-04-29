3ea7909e51427c962e0d9083b1cdcf03a70bf05d
andycall <dongtiangche@outlook.com>
2019-04-29 12:14:15 +0800

    docs: remove root container

--- examples/multi-container/src/index.js
+++ examples/multi-container/src/index.js
@@ -11,36 +11,32 @@ function App() {
     <div className="App">
       <RCREProvider store={store}>
         <h2>Use Multi Container Components as multi reducers</h2>
+        <p>Container has their own namespaces, components with the same name will not affect each other</p>
+        <div className={'demo1'}>
+          <div>demo 1 Container</div>
+          <Container model={'demo1'} data={{username: 'aaa'}}>
+            <span>UserName: </span>
+            <ES name={'username'}>{({$name, $value}, context) => (
+              <input value={$value || ''} onChange={event => context.container.$setData($name, event.target.value)}/>
+            )}</ES>
 
-        <Container model={'root'}>
-          <p>The Root Container</p>
-          <p>Container has their own namespaces, components with the same name will not affect each other</p>
-          <div className={'demo1'}>
-            <div>demo 1 Container</div>
-            <Container model={'demo1'} data={{username: 'aaa'}}>
-              <span>UserName: </span>
-              <ES name={'username'}>{({$name, $value}, context) => (
-                <input value={$value || ''} onChange={event => context.container.$setData($name, event.target.value)}/>
-              )}</ES>
-
-              <div>Components in the same container will share the same name</div>
-              <ES name={'username'}>{({$name, $value}, context) => (
-                <input value={$value || ''} onChange={event => context.container.$setData($name, event.target.value)}/>
-              )}</ES>
-              <div>demo1 state value: <ES>{({$data}) => <span>{JSON.stringify($data)}</span>}</ES></div>
-            </Container>
-          </div>
-          <div className={'demo2'}>
-            <div>demo 1 Container</div>
-            <Container model={'demo2'}>
-              <span>UserName: </span>
-              <ES name={'username'}>{({$name, $value}, context) => (
-                <input value={$value || ''} onChange={event => context.container.$setData($name, event.target.value)}/>
-              )}</ES>
-              <div>demo1 state value: <ES>{({$data}) => <span>{JSON.stringify($data)}</span>}</ES></div>
-            </Container>
-          </div>
-        </Container>
+            <div>Components in the same container will share the same name</div>
+            <ES name={'username'}>{({$name, $value}, context) => (
+              <input value={$value || ''} onChange={event => context.container.$setData($name, event.target.value)}/>
+            )}</ES>
+            <div>demo1 state value: <ES>{({$data}) => <span>{JSON.stringify($data)}</span>}</ES></div>
+          </Container>
+        </div>
+        <div className={'demo2'}>
+          <div>demo 1 Container</div>
+          <Container model={'demo2'}>
+            <span>UserName: </span>
+            <ES name={'username'}>{({$name, $value}, context) => (
+              <input value={$value || ''} onChange={event => context.container.$setData($name, event.target.value)}/>
+            )}</ES>
+            <div>demo1 state value: <ES>{({$data}) => <span>{JSON.stringify($data)}</span>}</ES></div>
+          </Container>
+        </div>
       </RCREProvider>
     </div>
   );
